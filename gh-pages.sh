01b9bfd820144a9fd7f5d91f88404fb08f1c890d
TxHawks <tofu.hawks@gmail.com>
2016-04-05 19:23:09 +0300

    Compile documentation

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Generate documentation and deploy it to GitHub pages
 # http://TxHawks.github.io/jigsass-objects-media/
-gulp sass:doc
 gulp sass:sg
 git add ./docs/.
 git commit -m "Compile documentation"
