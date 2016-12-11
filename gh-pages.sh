d92deb3b630c9a33305c4c1dc2ad07374e9b6426
TxHawks <tofu.hawks@gmail.com>
2016-12-11 14:32:45 +0200

    Compile documentation

--- gh-pages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Generate documentation and deploy it to GitHub pages
-# http://TxHawks.github.io/jigsass-objects-media/
-gulp sass:sg
-git add ./docs/.
-git commit -m "Compile documentation"
-git subtree push --prefix docs/styleguide origin gh-pages
--- test/helper.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Globals for all test_*.js files.
-global.assert = require('assert');
-global.fs = require('fs');
-global.path = require('path');
-global.Sassaby = require('sassaby');
